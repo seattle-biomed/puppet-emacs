f1890c481a84c584e86a29ca52bb03d358630881
Andy Leonard <andy.leonard@sbri.org>
2012-10-26 13:49:26 -0700

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+# == Class: emacs
+#
+# Manage emacs installation, including supporting packages.
+#
+# Currently only supports Ubuntu, tested only on 12.04.
+#
+# === Parameters
+#
+#
+# === Variables
+#
+#
+# === Examples
+#
+#  include emacs
+#
+# === Authors
+#
+# Andrew Leonard
+#
+# === Copyright
+#
+# Copyright 2012 Andrew Leonard, Seattle Biomedical Research Institute
+#
+class emacs {
+
+  $pkgs = [ 'emacs23-nox', 'puppet-el' ]
+
+  package { $pkgs: ensure => present }
+
+}
